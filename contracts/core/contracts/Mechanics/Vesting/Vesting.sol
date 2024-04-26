99ac2b00966be8a8df2c11c52bc1ed4fbd3c9734
Yury Toporov <gemuniongroup@gmail.com>
2024-04-26 15:30:44 +0800

    feat: approve vesting sale

--- contracts/core/contracts/Mechanics/Vesting/Vesting.sol
+++ contracts/core/contracts/Mechanics/Vesting/Vesting.sol
@@ -11,7 +11,7 @@ import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import { TopUp } from "../../utils/TopUp.sol";
 import { ExchangeUtils } from "../../Exchange/lib/ExchangeUtils.sol";
-import { Asset,TokenType,DisabledTokenTypes } from "../../Exchange/lib/interfaces/IAsset.sol";
+import { Asset, TokenType, DisabledTokenTypes } from "../../Exchange/lib/interfaces/IAsset.sol";
 import "./extension/WestingWalletApproved.sol";
 
 /**
@@ -23,60 +23,62 @@ import "./extension/WestingWalletApproved.sol";
  *      This contract abstracts all common functions and is used as an foundation for other vesting contracts
  */
 contract Vesting is VestingWalletApproved, TopUp {
-  using SafeCast for uint256;
+    using SafeCast for uint256;
 
-  uint64 public constant _monthInSeconds = 2592000; // The number of seconds in month
-  uint16 private immutable _cliffInMonth; // The number of days before the cliff period ends
-  uint16 private immutable _monthlyRelease; // The amount of tokens that can be released daily
+    uint64 public constant _monthInSeconds = 2592000; // The number of seconds in month
+    uint16 private immutable _cliffInMonth; // The number of days before the cliff period ends
+    uint16 private immutable _monthlyRelease; // The amount of tokens that can be released daily
 
-  constructor(
-    address beneficiary,
-    uint64 startTimestamp,
-    uint16 cliffInMonth,
-    uint16 monthlyRelease
-  ) VestingWalletApproved(beneficiary, startTimestamp, (10000 * _monthInSeconds) / monthlyRelease) {
-    _cliffInMonth = cliffInMonth;
-    _monthlyRelease = monthlyRelease;
-  }
+    constructor(
+        address beneficiary,
+        uint64 startTimestamp,
+        uint16 cliffInMonth,
+        uint16 monthlyRelease
+    ) VestingWalletApproved(beneficiary, startTimestamp, (10000 * _monthInSeconds) / monthlyRelease) {
+        _cliffInMonth = cliffInMonth;
+        _monthlyRelease = monthlyRelease;
+    }
 
-  /**
-   * @dev Computes the vesting schedule based on the total allocation and the timestamp.
+    /**
+     * @dev Computes the vesting schedule based on the total allocation and the timestamp.
    * @param totalAllocation The total allocation of tokens for vesting
    * @param timestamp The timestamp for which the vesting schedule is computed
    * @return The vesting schedule for the given total allocation and timestamp
    */
-  function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view override returns (uint256) {
-    uint256 _start = start() + _cliffInMonth * _monthInSeconds;
-    uint256 period = timestamp > _start ? (timestamp - _start) / _monthInSeconds : 0;
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view override returns (uint256) {
+        uint256 _start = start() + _cliffInMonth * _monthInSeconds;
+        uint256 period = timestamp > _start ? (timestamp - _start) / _monthInSeconds : 0;
 
-    if (timestamp < _start) {
-      return 0;
-    } else if (timestamp > _start + duration()) {
-      return totalAllocation;
-    } else {
-      return (totalAllocation * period * _monthlyRelease) / 10000;
+        if (timestamp < _start) {
+            return 0;
+        } else if (timestamp > _start + duration()) {
+            return totalAllocation;
+        } else {
+            return (totalAllocation * period * _monthlyRelease) / 10000;
+        }
     }
-  }
 
-  /**
-   * @dev Allows to top-up the vesting contract with tokens (NATIVE and ERC20 only)
+    /**
+     * @dev Allows to top-up the vesting contract with tokens (NATIVE and ERC20 only)
    * @param price An array of Asset representing the tokens to be transferred.
    */
-  function topUp(Asset[] memory price) external payable override {
-    ExchangeUtils.spendFrom(price, _msgSender(), address(this), DisabledTokenTypes(false, false, true, true, true));
-  }
+    function topUp(Asset[] memory price) external payable override {
+        ExchangeUtils.spendFrom(price, _msgSender(), address(this), DisabledTokenTypes(false, false, true, true, true));
+    }
 
-  /**
-   * @dev Restrict the contract to receive Ether (receive via topUp function only).
+    /**
+     * @dev Restrict the contract to receive Ether (receive via topUp function only).
    */
-  receive() external payable override(VestingWalletApproved, TopUp) {
-    revert();
-  }
+    receive() external payable override(VestingWalletApproved, TopUp) {
+        revert();
+    }
 
-  /**
-   * @dev See {IERC165-supportsInterface}.
+    /**
+     * @dev See {IERC165-supportsInterface}.
    */
-  function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-    return super.supportsInterface(interfaceId);
-  }
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
+
+
