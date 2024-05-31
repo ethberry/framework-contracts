c7fcf8aefe0ac94c89db8d0e3ba2cd2b9042677f
TrejGun <trejgun@gmail.com>
2024-05-31 19:11:57 +0800

    feat: disable chain link on unsupported networks

---
+++ contracts/core/test/ERC721/ERC721BlacklistRentable.spec.ts
@@ -0,0 +1,29 @@
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { shouldBehaveLikeAccessControl, shouldBehaveLikeBlackList } from "@gemunion/contracts-access";
+import { DEFAULT_ADMIN_ROLE, InterfaceId, METADATA_ROLE, MINTER_ROLE } from "@gemunion/contracts-constants";
+
+import { FrameworkInterfaceId } from "../constants";
+import { deployERC721 } from "./shared/fixtures";
+import { shouldBehaveLikeERC721Simple } from "./shared/simple";
+import { shouldBehaveLikeERC721Blacklist } from "./shared/blacklist";
+import { shouldBehaveLikeERC721Rentable } from "./shared/rentable";
+import { shouldMintCommon } from "./shared/simple/base/mintCommon";
+
+describe.only("ERC721BlacklistRentable", function () {
+  const factory = () => deployERC721(this.title);
+
+  shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE, METADATA_ROLE);
+  shouldBehaveLikeBlackList(factory);
+  shouldBehaveLikeERC721Blacklist(factory);
+  shouldBehaveLikeERC721Simple(factory);
+  shouldMintCommon(factory);
+
+  shouldBehaveLikeERC721Rentable(factory);
+
+  shouldSupportsInterface(factory)([
+    InterfaceId.IERC165,
+    InterfaceId.IAccessControl,
+    InterfaceId.IERC721,
+    InterfaceId.IERC4907,
+  ]);
+});
