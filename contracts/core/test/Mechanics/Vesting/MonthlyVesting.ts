28818c34e0c12b2e3e2bf2beb3ade1a7bc2feccc
TrejGun <trejgun@gmail.com>
2024-06-08 21:41:15 +0800

    feat: daily vesting

---
+++ contracts/core/test/Mechanics/Vesting/MonthlyVesting.ts
@@ -0,0 +1,58 @@
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { shouldBehaveLikeOwnable } from "@gemunion/contracts-access";
+import { InterfaceId } from "@gemunion/contracts-constants";
+
+import { deployVesting } from "./shared/fixture";
+import { calc } from "./shared/calc";
+import { shouldBehaveLikeTopUp } from "../../shared/topUp";
+
+describe("MonthlyVesting", function () {
+  const factory = () => deployVesting("MonthlyVesting", 12, 417);
+
+  shouldBehaveLikeOwnable(factory);
+  shouldBehaveLikeTopUp(factory);
+
+  describe("release", function () {
+    it("AdvisorsVesting", async function () {
+      await calc("MonthlyVesting", 12, 417);
+    });
+
+    it("MarketingVesting", async function () {
+      await calc("MonthlyVesting", 1, 1500);
+    });
+
+    it("PartnershipVesting", async function () {
+      await calc("MonthlyVesting", 6, 417);
+    });
+
+    it("PreSeedSaleVesting", async function () {
+      await calc("MonthlyVesting", 3, 416);
+    });
+
+    it("PrivateSaleVesting", async function () {
+      await calc("MonthlyVesting", 1, 624);
+    });
+
+    it("PublicSaleVesting", async function () {
+      await calc("MonthlyVesting", 0, 3333);
+    });
+
+    it("SeedSaleVesting", async function () {
+      await calc("MonthlyVesting", 2, 500);
+    });
+
+    it("TeamVesting", async function () {
+      await calc("MonthlyVesting", 12, 417);
+    });
+
+    it("InitialLiquidityVesting", async function () {
+      await calc("MonthlyVesting", 0, 5000);
+    });
+
+    it("TreasuryVesting", async function () {
+      await calc("MonthlyVesting", 3, 10000);
+    });
+  });
+
+  shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IERC1363Receiver, InterfaceId.IERC1363Spender]);
+});
