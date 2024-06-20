cba7143b22ff6ca5ed3cbe74eb87742121e98a22
JenyaFomin <jenya.stasik@gmail.com>
2024-06-19 20:50:22 -0400

    fix: inheritance for LootBoxBlacklistPausable

---
+++ contracts/core/contracts/Mechanics/LootBox/random/ERC721LootBoxBlacklistPausablePolygon.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Author: TrejGun
+// Email: trejgun@gemunion.io
+// Website: https://gemunion.io/
+
+pragma solidity ^0.8.20;
+
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { VRFConsumerBaseV2 } from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
+import { ChainLinkPolygonV2 } from "@gemunion/contracts-chain-link-v2/contracts/extensions/ChainLinkPolygonV2.sol";
+import { ChainLinkBaseV2 } from "@gemunion/contracts-chain-link-v2/contracts/extensions/ChainLinkBaseV2.sol";
+
+import { InvalidSubscription } from "../../../utils/errors.sol";
+import { ERC721LootBoxBlacklistPausable } from "../ERC721LootBoxBlacklistPausable.sol";
+import { ERC721LootBoxSimple } from "../ERC721LootBoxSimple.sol";
+import { ERC721LootBoxBlacklist } from "../ERC721LootBoxBlacklist.sol";
+
+contract ERC721LootBoxBlacklistPausablePolygon is ERC721LootBoxBlacklistPausable, ChainLinkPolygonV2 {
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint96 royalty,
+    string memory baseTokenURI
+  )
+    ERC721LootBoxBlacklistPausable(name, symbol, royalty, baseTokenURI)
+    ChainLinkPolygonV2(uint64(0), uint16(6), uint32(600000), uint32(1))
+  {}
+
+  // OWNER MUST SET A VRF SUBSCRIPTION ID AFTER DEPLOY
+  // event VrfSubscriptionSet(uint64 subId);
+  function setSubscriptionId(uint64 subId) public override onlyRole(DEFAULT_ADMIN_ROLE) {
+    if (subId == 0) {
+      revert InvalidSubscription();
+    }
+    _subId = subId;
+    emit VrfSubscriptionSet(_subId);
+  }
+
+  function getRandomNumber() internal override(ChainLinkBaseV2, ERC721LootBoxSimple) returns (uint256 requestId) {
+    if (_subId == 0) {
+      revert InvalidSubscription();
+    }
+    return super.getRandomNumber();
+  }
+
+  function fulfillRandomWords(
+    uint256 requestId,
+    uint256[] memory randomWords
+  ) internal override(ERC721LootBoxSimple, VRFConsumerBaseV2) {
+    return super.fulfillRandomWords(requestId, randomWords);
+  }
+
+  /**
+   * @dev See {IERC165-supportsInterface}.
+   */
+  function supportsInterface(
+    bytes4 interfaceId
+  ) public view virtual override(AccessControl, ERC721LootBoxBlacklist) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+}
