a60bd5767500c947d97bce2aa59c9bba407a1653
TrejGun <trejgun@gmail.com>
2024-07-04 13:53:13 +0100

    test: ponzi

--- contracts/core/test/Mechanics/Ponzi/fixture.ts
+++ contracts/core/test/Mechanics/Ponzi/fixture.ts
@@ -7,9 +7,3 @@ export async function deployPonzi(): Promise<any> {
   const factory = await ethers.getContractFactory("Ponzi");
   return factory.deploy([owner.address], [100]);
 }
-
-export async function deployPonziBasicRef(): Promise<any> {
-  const [owner] = await ethers.getSigners();
-  const factory = await ethers.getContractFactory("PonziBasicRef");
-  return factory.deploy([owner.address], [100]);
-}
