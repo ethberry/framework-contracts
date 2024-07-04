51f28347c404f475da285187f77ff692b533cc87
TrejGun <trejgun@gmail.com>
2024-07-04 13:53:00 +0100

    test: payment splitter

--- contracts/core/test/Mechanics/PaymentSplitter/PaymentSplitter.spec.ts
+++ contracts/core/test/Mechanics/PaymentSplitter/PaymentSplitter.spec.ts
@@ -1,8 +1,14 @@
 import { expect } from "chai";
 
 import { deployPaymentSplitter } from "./fixture";
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { InterfaceId } from "@gemunion/contracts-constants";
 
-describe("PaymentSplitter", function () {
+describe.only("PaymentSplitter", function () {
+  const factory = () => deployPaymentSplitter();
+
+  shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IERC1363Receiver, InterfaceId.IERC1363Spender]);
+
   describe("totalShares", function () {
     it("should get total shares", async function () {
       const contractInstance = await deployPaymentSplitter();
--- contracts/core/test/Mechanics/PaymentSplitter/fixture.ts
+++ contracts/core/test/Mechanics/PaymentSplitter/fixture.ts
@@ -2,8 +2,8 @@ import "@nomicfoundation/hardhat-toolbox";
 
 import { ethers } from "hardhat";
 
-export async function deployPaymentSplitter(): Promise<any> {
+export async function deployPaymentSplitter(name = "GemunionSplitter"): Promise<any> {
   const [owner] = await ethers.getSigners();
-  const factory = await ethers.getContractFactory("PaymentSplitter");
+  const factory = await ethers.getContractFactory(name);
   return factory.deploy([owner.address], [100]);
 }
