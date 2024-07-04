3ae3d47a1bd16c2460b1a1491a71030fbab37c4e
TrejGun <trejgun@gmail.com>
2024-07-04 10:34:41 +0100

    test: uncomment state hash tests

--- contracts/core/test/ERC998/ERC998StateHash.spec.ts
+++ contracts/core/test/ERC998/ERC998StateHash.spec.ts
@@ -1,12 +1,12 @@
 import { shouldSupportsInterface } from "@gemunion/contracts-utils";
 import { shouldBehaveLikeAccessControl } from "@gemunion/contracts-access";
 import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
+import { shouldBehaveLikeStateHash } from "@gemunion/contracts-erc998td";
 
 import { deployERC721 } from "../ERC721/shared/fixtures";
 import { shouldBehaveLikeERC998Simple } from "./shared/simple";
-// import { shouldStateHash } from "@gemunion/contracts-erc998td";
-// import { customMintCommonERC721 } from "../ERC721/shared/customMintFn";
-// import { tokenId } from "../constants";
+import { customMintCommonERC721 } from "../ERC721/shared/customMintFn";
+import { tokenId } from "../constants";
 
 describe("ERC998StateHash", function () {
   const factory = () => deployERC721(this.title);
@@ -14,7 +14,7 @@ describe("ERC998StateHash", function () {
   shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
 
   shouldBehaveLikeERC998Simple(factory);
-  // shouldStateHash(factory, { mint: customMintCommonERC721, tokenId });
+  shouldBehaveLikeStateHash(factory, { mint: customMintCommonERC721, tokenId });
 
   shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IAccessControl, InterfaceId.IERC721]);
 });
