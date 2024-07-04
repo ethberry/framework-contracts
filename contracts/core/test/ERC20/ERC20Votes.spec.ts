ecad82090ab333141f0716e9e96a8c782d09089d
TrejGun <trejgun@gmail.com>
2024-07-04 17:04:53 +0100

    test: erc20/erc721 votes

---
+++ contracts/core/test/ERC20/ERC20Votes.spec.ts
@@ -0,0 +1,21 @@
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
+import { shouldBehaveLikeAccessControl } from "@gemunion/contracts-access";
+
+import { shouldBehaveLikeERC20Simple } from "./shared/simple";
+import { deployERC1363 } from "./shared/fixtures";
+
+describe.only("ERC20Wotes", function () {
+  const factory = () => deployERC1363(this.title);
+
+  shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
+
+  shouldBehaveLikeERC20Simple(factory);
+
+  shouldSupportsInterface(factory)([
+    InterfaceId.IERC165,
+    InterfaceId.IAccessControl,
+    InterfaceId.IERC20,
+    InterfaceId.IERC1363,
+  ]);
+});
---
+++ contracts/core/test/ERC721/ERC721Votes.spec.ts
@@ -0,0 +1,19 @@
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { shouldBehaveLikeAccessControl } from "@gemunion/contracts-access";
+import { DEFAULT_ADMIN_ROLE, InterfaceId, MINTER_ROLE } from "@gemunion/contracts-constants";
+
+import { deployERC721 } from "./shared/fixtures";
+import { shouldBehaveLikeERC721Burnable } from "./shared/simple/burnable";
+import { shouldBehaveLikeERC721Soulbound } from "./shared/soulbound";
+import { shouldMintCommon } from "./shared/simple/base/mintCommon";
+
+describe("ERC721Votes", function () {
+  const factory = () => deployERC721(this.title);
+
+  shouldBehaveLikeAccessControl(factory)(DEFAULT_ADMIN_ROLE, MINTER_ROLE);
+
+  shouldMintCommon(factory);
+  shouldBehaveLikeERC721Burnable(factory);
+
+  shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IAccessControl, InterfaceId.IERC721]);
+});
