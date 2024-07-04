3e88b721eb3eacd9867fdaef0b5519bf181cc926
TrejGun <trejgun@gmail.com>
2024-07-04 16:47:25 +0100

    test: erc20 ownable

---
+++ contracts/core/test/ERC20/ERC20Ownable.spec.ts
@@ -0,0 +1,16 @@
+import { shouldSupportsInterface } from "@gemunion/contracts-utils";
+import { InterfaceId } from "@gemunion/contracts-constants";
+import { shouldBehaveLikeOwnable } from "@gemunion/contracts-access";
+
+import { shouldBehaveLikeERC20Simple } from "./shared/simple";
+import { deployERC1363 } from "./shared/fixtures";
+
+describe("ERC20Ownable", function () {
+  const factory = () => deployERC1363(this.title);
+
+  shouldBehaveLikeOwnable(factory);
+
+  shouldBehaveLikeERC20Simple(factory);
+
+  shouldSupportsInterface(factory)([InterfaceId.IERC165, InterfaceId.IERC20, InterfaceId.IERC1363]);
+});
