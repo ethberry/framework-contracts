3a20ee2ff244a04f685db6f405ce28eb9c859fd1
zaraman <31280006+zaraman@users.noreply.github.com>
2024-07-30 16:37:41 +0200

    add PoC

--- contracts/core/contracts/Exchange/DiamondExchange.sol
+++ contracts/core/contracts/Exchange/DiamondExchange.sol
@@ -6,9 +6,9 @@
 
 pragma solidity ^0.8.20;
 
-import { Diamond } from "../Diamond/Diamond.sol";
-import { LibDiamond } from "../Diamond/lib/LibDiamond.sol";
-import { AccessControlInternal } from "../Diamond/override/AccessControlInternal.sol";
+import {Diamond} from "../Diamond/Diamond.sol";
+import {LibDiamond} from "../Diamond/lib/LibDiamond.sol";
+import {AccessControlInternal} from "../Diamond/override/AccessControlInternal.sol";
 
 /**
  * todo create contract WalletDiamond. It have to be inherited by this contract.
@@ -18,4 +18,43 @@ contract DiamondExchange is Diamond, AccessControlInternal {
     // LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
     // Can initialise some storage values here if needed.
   }
+  
+}
+contract Deployer {
+    event Deployed(address addr);
+
+    function deploy(bytes32 salt, address _contractOwner, address _diamondCutFacet) public {
+        address addr;
+        bytes memory bytecode = abi.encodePacked(
+            type(DiamondExchange).creationCode,
+            abi.encode(_contractOwner, _diamondCutFacet)
+        );
+
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr);
+    }
+
+    function getAddress(bytes32 salt, address _contractOwner, address _diamondCutFacet) public view returns (address) {
+        bytes memory bytecode = abi.encodePacked(
+            type(DiamondExchange).creationCode,
+            abi.encode(_contractOwner, _diamondCutFacet)
+        );
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                salt,
+                keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint256(hash)));
+    }
 }
