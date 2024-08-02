5e4f1e7011fb2d09ceddce796caadaf31b54533f
zaraman <31280006+zaraman@users.noreply.github.com>
2024-08-02 18:31:38 +0200

    add:script and deployer for CM

--- contracts/core/contracts/ContractManager/DiamondCM.sol
+++ contracts/core/contracts/ContractManager/DiamondCM.sol
@@ -18,3 +18,41 @@ contract DiamondCM is Diamond, AccessControlInternal {
     // Can initialise some storage values here if needed.
   }
 }
+contract Deployer {
+    event Deployed(address addr);
+
+    function deploy(bytes32 salt, address _contractOwner, address _diamondCutFacet) public {
+        address addr;
+        bytes memory bytecode = abi.encodePacked(
+            type(DiamondCM).creationCode,
+            abi.encode(_contractOwner, _diamondCutFacet)
+        );
+
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+
+        emit Deployed(addr);
+    }
+
+    function getAddress(bytes32 salt, address _contractOwner, address _diamondCutFacet) public view returns (address) {
+        bytes memory bytecode = abi.encodePacked(
+            type(DiamondCM).creationCode,
+            abi.encode(_contractOwner, _diamondCutFacet)
+        );
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                salt,
+                keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint256(hash)));
+    }
+}
