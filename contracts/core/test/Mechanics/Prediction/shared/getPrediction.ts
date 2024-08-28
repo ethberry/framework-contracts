e810b137086480638295d13a4dc1f95d48067cdd
Igor Berlenko <igorberlenko7@gmail.com>
2024-08-28 12:34:07 +0800

    Remove getPrediction.ts and clean up comments

--- contracts/core/test/Mechanics/Prediction/shared/getPrediction.ts
+++
@@ -1,7 +0,0 @@
-export function shouldBetPosition(_predictionFactory: () => Promise<any>, _betAssetFactory: () => Promise<any>) {
-  describe("claimTreasury", function () {
-    it("should claim treasury", async function () {
-      // TODO implement
-    });
-  });
-}
