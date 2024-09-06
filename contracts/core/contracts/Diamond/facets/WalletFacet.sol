232273f5e8ca492a7bfe1df3f21279ab260609cd
TrejGun <trejgun@gmail.com>
2024-09-06 20:13:15 +0800

    fix: remove leftovers

--- contracts/core/contracts/Diamond/facets/WalletFacet.sol
+++ contracts/core/contracts/Diamond/facets/WalletFacet.sol
@@ -7,9 +7,8 @@
 pragma solidity ^0.8.20;
 
 import { ERC721Holder } from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import { ERC1155Holder } from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import { ERC1363Receiver } from "@gemunion/contracts-erc1363/contracts/extensions/ERC1363Receiver.sol";
-
-import { ERC1155Holder } from "../override/ERC1155Holder.sol";
 
 /**
  *@dev Wallet contract can receive ETH, ERC1363 tokens, ERC721 tokens, and ERC1155 tokens.
