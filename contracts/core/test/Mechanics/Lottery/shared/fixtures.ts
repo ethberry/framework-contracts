f6bdf2154e6eaa6979ea4d5d9fecd29266305f2a
Igor Berlenko <igorberlenko7@gmail.com>
2024-09-12 18:02:45 +0800

    10

--- contracts/core/test/Mechanics/Lottery/shared/fixtures.ts
+++ contracts/core/test/Mechanics/Lottery/shared/fixtures.ts
@@ -1,13 +1,35 @@
-import { ethers } from "hardhat";
-import { Contract } from "ethers";
+import "@nomicfoundation/hardhat-toolbox";
 
-export async function deployLotteryRandomContract(): Promise<Contract> {
-  const LotteryRandom = await ethers.getContractFactory("LotteryRandomHardhat");
-  const config = {
-    timeLagBeforeRelease: 2592000, // 30 days
-    commission: 30, // 30%
+import { ethers, network } from "hardhat";
+
+import { getContractName } from "../../../utils";
+import { deployERC721 } from "../../../ERC721/shared/fixtures";
+import { deployERC20 } from "../../../ERC20/shared/fixtures";
+import { wrapLotterySignature } from "./utils";
+
+interface ILotteryConfig {
+  timeLagBeforeRelease: number;
+  commission: number;
+}
+
+export async function deployLottery(config: ILotteryConfig): Promise<{
+  erc20Instance: any;
+  erc721Instance: any;
+  lotteryInstance: any;
+  generateSignature: any;
+}> {
+  const [owner] = await ethers.getSigners();
+  const factory = await ethers.getContractFactory(getContractName("LotteryRandom", network.name));
+
+  const erc20Instance: any = await deployERC20();
+  const erc721TicketInstance: any = await deployERC721("ERC721LotteryTicket");
+
+  const lotteryInstance: any = await factory.deploy(config);
+
+  return {
+    erc20Instance,
+    erc721Instance: erc721TicketInstance,
+    lotteryInstance,
+    generateSignature: wrapLotterySignature(await ethers.provider.getNetwork(), lotteryInstance, owner),
   };
-  const lottery = await LotteryRandom.deploy(config);
-  await lottery.deployed();
-  return lottery;
 }
