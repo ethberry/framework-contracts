a97028ee6852448e1d070a7125698bc7f9abadbe
Igor Berlenko <igorberlenko7@gmail.com>
2024-09-15 01:27:31 +0800

    31

--- contracts/core/test/Mechanics/LotteryRandom/shared/startRound.ts
+++ contracts/core/test/Mechanics/LotteryRandom/shared/startRound.ts
@@ -1,4 +1,3 @@
-```typescript
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { formatEther, ZeroAddress } from "ethers";
@@ -117,4 +116,3 @@ export function shouldStartRound(factory) {
     });
   });
 }
-```
