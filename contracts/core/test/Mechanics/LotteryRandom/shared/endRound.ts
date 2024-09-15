8e26930fd4c0eafe1eb0238813ebb2056743a81d
Igor Berlenko <igorberlenko7@gmail.com>
2024-09-15 15:04:31 +0800

    62

--- contracts/core/test/Mechanics/LotteryRandom/shared/endRound.ts
+++ contracts/core/test/Mechanics/LotteryRandom/shared/endRound.ts
@@ -1,10 +1,22 @@
-```typescript
 import { expect } from "chai";
+import { ethers, network } from "hardhat";
 import { formatEther, ZeroAddress } from "ethers";
 import { time } from "@openzeppelin/test-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { deployLinkVrfFixture } from "../../shared/link";
+import { randomRequest } from "../../shared/randomRequest";
 
 export function shouldEndRound(factory) {
   describe("endRound", function () {
+    let vrfInstance;
+    let subId;
+
+    before(async function () {
+      if (network.name === "hardhat") {
+        ({ vrfInstance, subId } = await loadFixture(deployLinkVrfFixture));
+      }
+    });
+
     it("should end the current round", async function () {
       const lotteryInstance = await factory();
 
@@ -25,6 +37,16 @@ export function shouldEndRound(factory) {
       // Start the first round
       await lotteryInstance.startRound(ticket, price, 100);
 
+      if (network.name === "hardhat") {
+        // Set VRFV2 Subscription
+        const tx01 = lotteryInstance.setSubscriptionId(subId);
+        await expect(tx01).to.emit(lotteryInstance, "VrfSubscriptionSet").withArgs(subId);
+
+        // Add Consumer to VRFV2
+        const tx02 = vrfInstance.addConsumer(subId, lotteryInstance);
+        await expect(tx02).to.emit(vrfInstance, "SubscriptionConsumerAdded").withArgs(subId, lotteryInstance);
+      }
+
       // End the current round
       const tx = lotteryInstance.endRound();
       const currentTimestamp = (await time.latest()).toNumber();
@@ -32,6 +54,10 @@ export function shouldEndRound(factory) {
 
       const roundInfo = await lotteryInstance.getCurrentRoundInfo();
       expect(roundInfo.endTimestamp).to.be.gte(currentTimestamp);
+
+      if (network.name === "hardhat") {
+        await randomRequest(lotteryInstance, vrfInstance);
+      }
     });
 
     it("should fail: LotteryWrongRound", async function () {
@@ -61,6 +87,16 @@ export function shouldEndRound(factory) {
       // Start the first round
       await lotteryInstance.startRound(ticket, price, 100);
 
+      if (network.name === "hardhat") {
+        // Set VRFV2 Subscription
+        const tx01 = lotteryInstance.setSubscriptionId(subId);
+        await expect(tx01).to.emit(lotteryInstance, "VrfSubscriptionSet").withArgs(subId);
+
+        // Add Consumer to VRFV2
+        const tx02 = vrfInstance.addConsumer(subId, lotteryInstance);
+        await expect(tx02).to.emit(vrfInstance, "SubscriptionConsumerAdded").withArgs(subId, lotteryInstance);
+      }
+
       // End the current round
       await lotteryInstance.endRound();
 
@@ -68,32 +104,5 @@ export function shouldEndRound(factory) {
       const tx = lotteryInstance.endRound();
       await expect(tx).to.be.revertedWithCustomError(lotteryInstance, "LotteryRoundNotActive");
     });
-
-    it("should fail: AccessControlUnauthorizedAccount", async function () {
-      const lotteryInstance = await factory();
-      const [_, nonAdmin] = await ethers.getSigners();
-
-      const ticket = {
-        tokenType: 2, // ERC721 TokenType
-        token: ZeroAddress,
-        tokenId: 0n,
-        amount: 1n,
-      };
-
-      const price = {
-        tokenType: 1, // ERC20 TokenType
-        token: ZeroAddress,
-        tokenId: 0n,
-        amount: 1n,
-      };
-
-      // Start the first round
-      await lotteryInstance.startRound(ticket, price, 100);
-
-      // Attempt to end the round with a non-admin account
-      const tx = lotteryInstance.connect(nonAdmin).endRound();
-      await expect(tx).to.be.revertedWithCustomError(lotteryInstance, "AccessControlUnauthorizedAccount");
-    });
   });
 }
-```
